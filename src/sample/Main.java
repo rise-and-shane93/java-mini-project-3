1f1adb7501cadbd589467c1c370fc858567d2a9b
Shane Harper <cdtharper@gmail.com>
2021-05-03 23:01:05 -0400

    user can calculate interest rate

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,11 +1,15 @@
 package sample;
 
 import javafx.application.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.*;
 import javafx.stage.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
+import javafx.event.*;
+import java.lang.*;
 
 public class Main extends Application {
 
@@ -40,6 +44,7 @@ public class Main extends Application {
         monthlyTextArea = new TextField();
         totalTextArea = new TextField();
         calcBtn = new Button("Calculate");
+        calcBtn.setOnAction(new ButtonHandler());
 
         totalTextArea.setEditable(false);
         rootNode.add(interestRateLabel, 0,0);
@@ -48,8 +53,6 @@ public class Main extends Application {
         rootNode.add(numYearsTextArea, 1,1);
         rootNode.add(amountLabel, 0,2);
         rootNode.add(amountTextArea, 1,2);
-//        rootNode.add(amountLabel, 0,3);
-//        rootNode.add(amountTextArea, 1,3);
         rootNode.add(monthlyLabel, 0,3);
         rootNode.add(monthlyTextArea, 1,3);
         rootNode.add(totalLabel, 0,4);
@@ -62,5 +65,46 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    class ButtonHandler implements EventHandler<ActionEvent> {
+        public double amountDbl = 0;
+        public double interestRateDbl = 0;
+        public double numYearsDbl = 0;
+        public double monthlyDbl = 0;
+        public double totalDbl = 0;
+        public double monthlyInterestRateDbl = 0;
+
+        public double monthlyDblRound = 0;
+        public double totalDblRound = 0;
+
+        public String amountStr = "";
+        public String interestRateStr = "";
+        public String numYearsStr = "";
+        public String monthlyStr = "";
+        public String totalStr = "";
+
+        public void handle(ActionEvent e) {
+            amountStr = amountTextArea.getText();
+            amountDbl = Double.parseDouble(amountStr);
+
+            interestRateStr = interestRateTextArea.getText();
+            interestRateDbl = Double.parseDouble(interestRateStr);
+
+            numYearsStr = numYearsTextArea.getText();
+            numYearsDbl = Double.parseDouble(numYearsStr);
+
+            calcBtn.setText("Clicked");
+
+            monthlyInterestRateDbl = (interestRateDbl/100)/12;
+            monthlyDbl = ((monthlyInterestRateDbl * amountDbl)/(1-(Math.pow((1+monthlyInterestRateDbl), (numYearsDbl * -12)))));
+            totalDbl = monthlyDbl * (numYearsDbl * 12);
+            monthlyDblRound = Math.round(monthlyDbl * 100.0) / 100.0;
+            totalDblRound = Math.round(totalDbl * 100.0) / 100.0;
+//            testing = 1-(Math.pow((1+.00417), -24));
+//            testing = (.05/12) * 2000;
+            monthlyTextArea.setText(String.valueOf(monthlyDblRound));
+            totalTextArea.setText(String.valueOf(totalDblRound));
+        }
     }
 }
