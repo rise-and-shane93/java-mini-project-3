1492a2f6474fd583b16b94b9e3053b95fa4b6fda
Shane Harper <cdtharper@gmail.com>
2021-05-09 15:02:31 -0400

    final commit before turning in project

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,10 +5,10 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.*;
+import javafx.scene.text.Text;
 import javafx.stage.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
-import javafx.event.*;
 import java.lang.*;
 
 public class Main extends Application {
@@ -24,6 +24,7 @@ public class Main extends Application {
     private TextField monthlyTextArea;
     private TextField totalTextArea;
     private Button calcBtn;
+    private Text messageText;
 
     @Override
     public void start(Stage myStage) throws Exception{
@@ -32,7 +33,7 @@ public class Main extends Application {
         rootNode.setHgap(5);
         rootNode.setVgap(5);
         rootNode.setAlignment(Pos.CENTER);
-        Scene myScene = new Scene(rootNode, 400, 300);
+        Scene myScene = new Scene(rootNode, 400, 325);
         interestRateLabel = new Label("Annual Interest Rate:");
         numYearsLabel = new Label("Number of Years:");
         amountLabel = new Label("Loan Amount:");
@@ -44,8 +45,16 @@ public class Main extends Application {
         monthlyTextArea = new TextField();
         totalTextArea = new TextField();
         calcBtn = new Button("Calculate");
+        messageText = new Text("");
         calcBtn.setOnAction(new ButtonHandler());
+        monthlyTextArea.setOnMouseClicked(e -> {
+            messageText.setText("This text field will display\nthe calculation's result.");
+        });
+        totalTextArea.setOnMouseClicked(e -> {
+            messageText.setText("This text field will display\nthe calculation's result.");
+        });
 
+        monthlyTextArea.setEditable(false);
         totalTextArea.setEditable(false);
         rootNode.add(interestRateLabel, 0,0);
         rootNode.add(interestRateTextArea, 1,0);
@@ -58,6 +67,7 @@ public class Main extends Application {
         rootNode.add(totalLabel, 0,4);
         rootNode.add(totalTextArea, 1,4);
         rootNode.add(calcBtn, 1,5);
+        rootNode.add(messageText, 1, 6);
         myStage.setScene(myScene);
         myStage.show();
     }
@@ -78,33 +88,33 @@ public class Main extends Application {
         public double monthlyDblRound = 0;
         public double totalDblRound = 0;
 
-        public String amountStr = "";
-        public String interestRateStr = "";
-        public String numYearsStr = "";
-        public String monthlyStr = "";
-        public String totalStr = "";
+//        public String amountStr = "";
+//        public String interestRateStr = "";
+//        public String numYearsStr = "";
 
         public void handle(ActionEvent e) {
-            amountStr = amountTextArea.getText();
-            amountDbl = Double.parseDouble(amountStr);
 
-            interestRateStr = interestRateTextArea.getText();
-            interestRateDbl = Double.parseDouble(interestRateStr);
+            try {
+//                amountStr = amountTextArea.getText();
+//                interestRateStr = interestRateTextArea.getText();
+//                numYearsStr = numYearsTextArea.getText();
 
-            numYearsStr = numYearsTextArea.getText();
-            numYearsDbl = Double.parseDouble(numYearsStr);
+                amountDbl = Double.parseDouble(amountTextArea.getText());
+                interestRateDbl = Double.parseDouble(interestRateTextArea.getText());
+                numYearsDbl = Double.parseDouble(numYearsTextArea.getText());
 
-            calcBtn.setText("Clicked");
+                monthlyInterestRateDbl = (interestRateDbl/100)/12;
+                monthlyDbl = ((monthlyInterestRateDbl * amountDbl)/(1-(Math.pow((1+monthlyInterestRateDbl), (numYearsDbl * -12)))));
+                totalDbl = monthlyDbl * (numYearsDbl * 12);
+                monthlyDblRound = Math.round(monthlyDbl * 100.0) / 100.0;
+                totalDblRound = Math.round(totalDbl * 100.0) / 100.0;
+                monthlyTextArea.setText("$ " + String.valueOf(monthlyDblRound));
+                totalTextArea.setText("$ " + String.valueOf(totalDblRound));
+                messageText.setText("Here are the monthly payment\nand total payment amounts.");
+            } catch (NumberFormatException err) {
+                messageText.setText("Incomplete or incorrect data.\nPlease fill out the fields\nfor the interest rate,\nnumber of years and\nloan amount then try again.");
+            }
 
-            monthlyInterestRateDbl = (interestRateDbl/100)/12;
-            monthlyDbl = ((monthlyInterestRateDbl * amountDbl)/(1-(Math.pow((1+monthlyInterestRateDbl), (numYearsDbl * -12)))));
-            totalDbl = monthlyDbl * (numYearsDbl * 12);
-            monthlyDblRound = Math.round(monthlyDbl * 100.0) / 100.0;
-            totalDblRound = Math.round(totalDbl * 100.0) / 100.0;
-//            testing = 1-(Math.pow((1+.00417), -24));
-//            testing = (.05/12) * 2000;
-            monthlyTextArea.setText(String.valueOf(monthlyDblRound));
-            totalTextArea.setText(String.valueOf(totalDblRound));
         }
     }
 }
